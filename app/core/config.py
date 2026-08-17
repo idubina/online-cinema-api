f9c248049b89048c8550335f9096c9c94228f366
Illia Dubina <armahanz.music@gmail.com>
2026-08-17 16:32:48 +0300

    test: configure test database environment

--- app/core/config.py
+++ app/core/config.py
@@ -4,11 +4,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI backend for online cinema"
 
-    POSTGRES_DB: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
+
+    POSTGRES_DB: str
     POSTGRES_HOST: str = "localhost"
     POSTGRES_PORT: int = 5432
+
+    POSTGRES_TEST_DB: str
+    POSTGRES_TEST_HOST: str
+    POSTGRES_TEST_PORT: int
 
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -24,6 +29,17 @@ class Settings(BaseSettings):
             f"{self.POSTGRES_HOST}:"
             f"{self.POSTGRES_PORT}/"
             f"{self.POSTGRES_DB}"
+        )
+
+    @property
+    def async_sqlalchemy_database_test_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_TEST_HOST}:"
+            f"{self.POSTGRES_TEST_PORT}/"
+            f"{self.POSTGRES_TEST_DB}"
         )
 
     @property
