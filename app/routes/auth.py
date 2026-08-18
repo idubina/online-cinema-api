c1c9472733b764fb136bf59a29b369e8f52787a1
Illia Dubina <armahanz.music@gmail.com>
2026-08-18 15:07:23 +0300

    feat: add user activation route

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -15,3 +15,17 @@ async def register_user(
     db: SessionDep, user_data: auth_schemas.UserRegistrationRequestSchema
 ):
     return await auth_services.create_user(db=db, user_data=user_data)
+
+
+@router.post(
+    "/activate/",
+    response_model=auth_schemas.MessageResponseSchema,
+)
+async def activate_user(
+    db: SessionDep, user_data: auth_schemas.UserActivationRequestSchema
+):
+    await auth_services.activate_user(db=db, user_data=user_data)
+    message = auth_schemas.MessageResponseSchema(
+        message="User account activated successfully."
+    )
+    return message
