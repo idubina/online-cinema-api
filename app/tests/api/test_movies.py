5b805c3737f5a2ccb0e99aa13d7bb634ba56caad
Illia Dubina <armahanz.music@gmail.com>
2026-08-21 12:50:15 +0300

    test: add read movie tests

--- app/tests/api/test_movies.py
+++ app/tests/api/test_movies.py
@@ -208,22 +208,69 @@ async def test_create_movie_with_same_date_and_name_error(
         json=movie_payload_full,
     )
 
-    assert response_1.status_code == 201
+    async def test_read_movie_success(
+        client: AsyncClient, db_session: AsyncSession, authenticated_user
+    ):
+        user, access_token = authenticated_user
+        current_user = await db_session.scalar(
+            select(UserModel).where(UserModel.id == user["id"])
+        )
 
-    response_2 = await client.post(
-        f"{CINEMA_URL}/movies/",
-        headers={
-            "Authorization": f"Bearer {access_token}",
-        },
-        json=movie_payload_full,
-    )
+        current_user.group = await db_session.scalar(
+            select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.MODERATOR)
+        )
+        db_session.add(current_user)
+        await db_session.commit()
 
-    assert response_2.status_code == 409
+        movie_payload_1 = {
+            "name": "Test",
+            "date": "2020-08-20",
+            "score": 89,
+            "overview": "Tested",
+            "status": "Released",
+            "budget": "12345654",
+            "revenue": "76543245",
+        }
 
-    detail = response_2.json()["detail"]
-    movie_name = movie_payload_full["name"]
-    movie_date = movie_payload_full["date"]
-    assert (
-        detail
-        == f"A movie with the name '{movie_name}' and release date '{movie_date}' already exists."
-    )
+        movie_payload_2 = {
+            "country": "USA",
+            "genres": ["Test Genre"],
+            "actors": ["Test Actor 1", "Test Actor 2"],
+            "languages": ["Test Language"],
+        }
+
+        movie_payload_full = movie_payload_1 | movie_payload_2
+
+        create_response = await client.post(
+            f"{CINEMA_URL}/movies/",
+            headers={
+                "Authorization": f"Bearer {access_token}",
+            },
+            json=movie_payload_full,
+        )
+
+        assert create_response.status_code == 201
+
+        create_response_data = create_response.json()
+
+        read_response = await client.get(
+            f"{CINEMA_URL}/movies/{create_response_data['id']}/"
+        )
+
+        assert read_response.status_code == 200
+
+        read_response_data = read_response.json()
+
+        assert read_response_data == create_response_data
+
+    async def test_read_movie_not_found_error(
+        client: AsyncClient, db_session: AsyncSession
+    ):
+
+        read_response = await client.get(f"{CINEMA_URL}/movies/1/")
+
+        assert read_response.status_code == 404
+
+        detail = read_response.json()["detail"]
+
+        assert detail == "Movie with the given ID was not found."
