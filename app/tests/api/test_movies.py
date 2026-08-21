56725411bf788aba657e6146742d934c5f15cd50
Illia Dubina <armahanz.music@gmail.com>
2026-08-21 14:15:38 +0300

    test: add read all movies tests

--- app/tests/api/test_movies.py
+++ app/tests/api/test_movies.py
@@ -1,3 +1,4 @@
+import math
 from datetime import date, timedelta
 
 import pytest
@@ -275,3 +276,161 @@ async def test_read_movie_not_found_error(
     detail = read_response.json()["detail"]
 
     assert detail == "Movie with the given ID was not found."
+
+
+async def test_read_all_movie_success(
+    client: AsyncClient, db_session: AsyncSession, authenticated_user
+):
+    user, access_token = authenticated_user
+    current_user = await db_session.scalar(
+        select(UserModel).where(UserModel.id == user["id"])
+    )
+
+    current_user.group = await db_session.scalar(
+        select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.MODERATOR)
+    )
+    db_session.add(current_user)
+    await db_session.commit()
+
+    movie_payload_1 = {
+        "date": "2020-08-20",
+        "score": 89,
+        "overview": "Tested",
+        "status": "Released",
+        "budget": "12345654",
+        "revenue": "76543245",
+    }
+
+    movie_payload_2 = {
+        "country": "USA",
+        "genres": ["Test Genre"],
+        "actors": ["Test Actor 1", "Test Actor 2"],
+        "languages": ["Test Language"],
+    }
+
+    movie_payload_full = movie_payload_1 | movie_payload_2
+    total_items = 14
+
+    for i in range(1, total_items + 1):
+        movie_payload = movie_payload_full | {
+            "name": f"Test {i}",
+        }
+
+        response = await client.post(
+            f"{CINEMA_URL}/movies/",
+            headers={
+                "Authorization": f"Bearer {access_token}",
+            },
+            json=movie_payload,
+        )
+
+        assert response.status_code == 201
+
+    per_page = 10
+    page = 1
+    total_pages = math.ceil(total_items / per_page)
+
+    response = await client.get(f"{CINEMA_URL}/movies/?page={page}&per_page={per_page}")
+
+    assert response.status_code == 200
+
+    details = response.json()
+
+    assert len(details["movies"]) == per_page
+    assert details["total_items"] == total_items
+    assert details["total_pages"] == total_pages
+    assert details["prev_page"] is None
+
+    next_page_number = page + 1
+
+    assert (
+        details["next_page"]
+        == f"{CINEMA_URL}/movies/?page={next_page_number}&per_page={per_page}"
+    )
+
+    response = await client.get(details["next_page"])
+
+    assert response.status_code == 200
+
+    details = response.json()
+
+    assert len(details["movies"]) == total_items - per_page
+    assert details["total_items"] == total_items
+    assert details["total_pages"] == total_pages
+    assert details["next_page"] is None
+
+    assert (
+        details["prev_page"] == f"{CINEMA_URL}/movies/?page={page}&per_page={per_page}"
+    )
+
+
+async def test_get_movie_list_empty(client: AsyncClient):
+    response = await client.get(f"{CINEMA_URL}/movies/")
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "No movies found."
+
+
+async def test_get_movie_list_page_not_found(
+    client: AsyncClient,
+    db_session: AsyncSession,
+    authenticated_user,
+):
+    user, access_token = authenticated_user
+
+    current_user = await db_session.scalar(
+        select(UserModel).where(UserModel.id == user["id"])
+    )
+
+    current_user.group = await db_session.scalar(
+        select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.MODERATOR)
+    )
+
+    await db_session.commit()
+
+    movie_payload = {
+        "name": "Test",
+        "date": "2020-08-20",
+        "score": 89,
+        "overview": "Tested",
+        "status": "Released",
+        "budget": "12345654",
+        "revenue": "76543245",
+        "country": "USA",
+        "genres": ["Test Genre"],
+        "actors": ["Test Actor"],
+        "languages": ["Test Language"],
+    }
+
+    response = await client.post(
+        f"{CINEMA_URL}/movies/",
+        headers={
+            "Authorization": f"Bearer {access_token}",
+        },
+        json=movie_payload,
+    )
+
+    assert response.status_code == 201
+
+    response = await client.get(f"{CINEMA_URL}/movies/?page=2&per_page=10")
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "No movies found."
+
+
+@pytest.mark.parametrize(
+    "query",
+    [
+        "page=0&per_page=10",
+        "page=-1&per_page=10",
+        "page=1&per_page=0",
+        "page=1&per_page=101",
+    ],
+)
+async def test_get_movie_list_invalid_pagination(
+    client: AsyncClient,
+    query: str,
+):
+    response = await client.get(f"{CINEMA_URL}/movies/?{query}")
+
+    assert response.status_code == 422
